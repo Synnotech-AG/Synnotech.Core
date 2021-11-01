38c001d2af6ce2c9ecc70b5a87977c5a4542c3c1
Kenny Pflug <kenny.pflug@live.de>
2021-11-01 10:06:09 +0100

    added additional paragraph about when to not use DoubleParser.TryParse

--- Code/Synnotech.Core/Parsing/DoubleParser.cs
+++ Code/Synnotech.Core/Parsing/DoubleParser.cs
@@ -10,11 +10,22 @@ namespace Synnotech.Core.Parsing;
 public static class DoubleParser
 {
     /// <summary>
+    /// <para>
     /// Tries to parse the specified text to a double value. This method will
     /// check if the text uses the point character '.' or the comma character ','
     /// as the decimal sign and will parse the text either with the invariant culture
     /// or the German culture. <see cref="NumberStyles.Number" /> is used when parsing
     /// the text.
+    /// </para>
+    /// <para>
+    /// BE CAREFUL: if you have a number with only a single thousand-delimiter sign (i.e. no
+    /// decimal sign), this number will not be parsed correctly. The thousand-delimiter
+    /// sign will be interpreted as the decimal sign by this method. We recognize that
+    /// this scenario is rare, as especially human input will most likely never use
+    /// the thousand-delimiter sign, but if you happen to have this scenario, please
+    /// use <see cref="double.TryParse(string, NumberStyles, IFormatProvider, out double)"/>
+    /// instead and specify the corresponding culture info.
+    /// </para>
     /// </summary>
     /// <param name="text">The text to be parsed.</param>
     /// <param name="value">The parsed double value.</param>
@@ -23,13 +34,24 @@ public static class DoubleParser
         TryParse(text, NumberStyles.Number, out value);
 
     /// <summary>
+    /// <para>
     /// Tries to parse the specified text to a double value. This method will
     /// check if the text uses the point character '.' or the comma character ','
     /// as the decimal sign and will parse the text either with the invariant culture
     /// or the German culture.
+    /// </para>
+    /// <para>
+    /// BE CAREFUL: if you have a number with only a single thousand-delimiter sign (i.e. no
+    /// decimal sign), this number will not be parsed correctly. The thousand-delimiter
+    /// sign will be interpreted as the decimal sign by this method. We recognize that
+    /// this scenario is rare, as especially human input will most likely never use
+    /// the thousand-delimiter sign, but if you happen to have this scenario, please
+    /// use <see cref="double.TryParse(string, NumberStyles, IFormatProvider, out double)"/>
+    /// instead and specify the corresponding culture info.
+    /// </para>
     /// </summary>
     /// <param name="text">The text to be parsed.</param>
-    /// <param name="style">The number style that describes what the parser has to handle.</param>
+    /// <param name="style">The number style that describes how the parser will handle the text.</param>
     /// <param name="value">The parsed double value.</param>
     /// <returns>True if parsing was successful, else false.</returns>
     public static bool TryParse(string text, NumberStyles style, out double value)
