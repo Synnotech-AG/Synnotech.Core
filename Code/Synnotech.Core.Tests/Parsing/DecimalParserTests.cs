e3146debf6567107b87d88b4662d279af0f8aeb5
Kenny Pflug <kenny.pflug@live.de>
2021-11-02 10:02:50 +0100

    added DecimalParser

---
+++ Code/Synnotech.Core.Tests/Parsing/DecimalParserTests.cs
@@ -0,0 +1,73 @@
+﻿using FluentAssertions;
+using Synnotech.Core.Parsing;
+using Xunit;
+
+namespace Synnotech.Core.Tests.Parsing;
+
+public static class DecimalParserTests
+{
+    [Theory]
+    [MemberData(nameof(DecimalPointData))]
+    public static void ParseFloatingPointNumberWithDecimalPoint(string text, decimal expectedValue) =>
+        CheckNumber(text, expectedValue);
+
+    public static readonly TheoryData<string, decimal> DecimalPointData =
+        new ()
+        {
+            { "0.74", 0.74m },
+            { "1.34", 1.34m },
+            { "391202.9", 391202.9m },
+            { "-20.816", -20.816m },
+            { "15,019.33", 15_019.33m }
+        };
+
+    [Theory]
+    [MemberData(nameof(DecimalCommaData))]
+    public static void ParseFloatingPointNumberWithDecimalComma(string text, decimal expectedValue) =>
+        CheckNumber(text, expectedValue);
+
+    public static readonly TheoryData<string, decimal> DecimalCommaData =
+        new ()
+        {
+            { "000,7832", 0.7832m },
+            { "-0,499", -0.499m },
+            { "40593,84", 40593.84m },
+            { "1.943.100,84", 1_943_100.84m }
+        };
+
+    [Theory]
+    [MemberData(nameof(IntegerData))]
+    public static void ParseInteger(string text, decimal expectedValue) =>
+        CheckNumber(text, expectedValue);
+
+    public static readonly TheoryData<string, decimal> IntegerData =
+        new ()
+        {
+            { "15", 15.0m },
+            { "-743923", -743923.0m },
+            { "239.482.392.923", 239_482_392_923.0m },
+            { "21,500,000", 21_500_000.0m }
+        };
+
+    private static void CheckNumber(string text, decimal expectedValue)
+    {
+        var result = DecimalParser.TryParse(text, out var parsedValue);
+
+        result.Should().BeTrue();
+        parsedValue.Should().Be(expectedValue);
+    }
+
+    [Theory]
+    [InlineData("Foo")]
+    [InlineData("Bar")]
+    [InlineData("")]
+    [InlineData(null)]
+    [InlineData("9392gk381")]
+    public static void InvalidNumber(string text)
+    {
+        var result = DecimalParser.TryParse(text, out var actualValue);
+
+        result.Should().BeFalse();
+        actualValue.Should().Be(default);
+    }
+}
---
+++ Code/Synnotech.Core/Parsing/DecimalParser.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Globalization;
+using Light.GuardClauses;
+
+namespace Synnotech.Core.Parsing;
+
+/// <summary>
+/// Provides methods for parsing <see cref="decimal" /> values.
+/// </summary>
+public static class DecimalParser
+{
+    /// <summary>
+    /// <para>
+    /// Tries to parse the specified text to a decimal value. This method will
+    /// check if the text uses the point character '.' or the comma character ','
+    /// as the decimal sign and will parse the text either with the invariant culture
+    /// or the German culture. <see cref="NumberStyles.Number" /> is used when parsing
+    /// the text.
+    /// </para>
+    /// <para>
+    /// BE CAREFUL: if you have a number with only a single thousand-delimiter sign (i.e. no
+    /// decimal sign), this number will not be parsed correctly. The thousand-delimiter
+    /// sign will be interpreted as the decimal sign by this method. We recognize that
+    /// this scenario is rare, as especially human input will most likely never use
+    /// the thousand-delimiter sign, but if you happen to have this scenario, please
+    /// use <see cref="decimal.TryParse(string, NumberStyles, IFormatProvider, out decimal)" />
+    /// instead and specify the corresponding culture info.
+    /// </para>
+    /// </summary>
+    /// <param name="text">The text to be parsed.</param>
+    /// <param name="value">The parsed decimal value.</param>
+    /// <returns>True if parsing was successful, else false.</returns>
+    public static bool TryParse(string text, out decimal value) =>
+        TryParse(text, NumberStyles.Number, out value);
+
+    /// <summary>
+    /// <para>
+    /// Tries to parse the specified text to a decimal value. This method will
+    /// check if the text uses the point character '.' or the comma character ','
+    /// as the decimal sign and will parse the text either with the invariant culture
+    /// or the German culture.
+    /// </para>
+    /// <para>
+    /// BE CAREFUL: if you have a number with only a single thousand-delimiter sign (i.e. no
+    /// decimal sign), this number will not be parsed correctly. The thousand-delimiter
+    /// sign will be interpreted as the decimal sign by this method. We recognize that
+    /// this scenario is rare, as especially human input will most likely never use
+    /// the thousand-delimiter sign, but if you happen to have this scenario, please
+    /// use <see cref="decimal.TryParse(string, NumberStyles, IFormatProvider, out decimal)" />
+    /// instead and specify the corresponding culture info.
+    /// </para>
+    /// </summary>
+    /// <param name="text">The text to be parsed.</param>
+    /// <param name="style">The number style that describes how the parser will handle the text.</param>
+    /// <param name="value">The parsed decimal value.</param>
+    /// <returns>True if parsing was successful, else false.</returns>
+    public static bool TryParse(string text, NumberStyles style, out decimal value)
+    {
+        if (text.IsNullOrWhiteSpace())
+        {
+            value = default;
+            return false;
+        }
+
+        var result = FloatingPointAnalysis.AnalyseText(text.AsSpan());
+        var cultureInfo = result.ChooseCultureInfo();
+        return decimal.TryParse(text, style, cultureInfo, out value);
+    }
+}
