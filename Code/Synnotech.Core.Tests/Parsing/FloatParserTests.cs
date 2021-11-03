5415fa6ff3711cde1dabd39045d4dfb221e6055f
Kenny Pflug <kenny.pflug@live.de>
2021-11-03 06:57:14 +0100

    added ReadOnlySpan<char> overloads to FloatParser

--- Code/Synnotech.Core.Tests/Parsing/FloatParserTests.cs
+++ Code/Synnotech.Core.Tests/Parsing/FloatParserTests.cs
@@ -1,4 +1,5 @@
-﻿using FluentAssertions;
+﻿using System;
+using FluentAssertions;
 using Synnotech.Core.Parsing;
 using Xunit;
 
@@ -9,29 +10,62 @@ public static class FloatParserTests
     private const float Precision = 0.000001f;
 
     [Theory]
-    [InlineData("0.13", 0.13f)]
-    [InlineData("-3.41", -3.41f)]
-    [InlineData("10050.9", 10050.9f)]
-    [InlineData("-394.955", -394.955f)]
-    [InlineData("15,019.33", 15_019.33f)]
+    [MemberData(nameof(NumbersWithDecimalPoint))]
     public static void ParseFloatingPointNumberWithDecimalPoint(string text, float expectedValue) =>
         CheckNumber(text, expectedValue);
 
     [Theory]
-    [InlineData("000,7832", 0.7832f)]
-    [InlineData("-0,499", -0.499f)]
-    [InlineData("40593,84", 40593.84f)]
-    [InlineData("1.943.100,84", 1_943_100.84f)]
+    [MemberData(nameof(NumbersWithDecimalPoint))]
+    public static void ParseFloatingPointNumberWithDecimalPointAsSpan(string text, float expectedValue) =>
+        CheckNumberAsSpan(text, expectedValue);
+
+    public static readonly TheoryData<string, float> NumbersWithDecimalPoint =
+        new ()
+        {
+            { "0.13", 0.13f },
+            { "-3.41", -3.41f },
+            { "10050.9", 10050.9f },
+            { "-394.955", -394.955f },
+            { "15,019.33", 15_019.33f }
+        };
+
+    [Theory]
+    [MemberData(nameof(NumbersWithDecimalComma))]
     public static void ParseFloatingPointNumberWithDecimalComma(string text, float expectedValue) =>
         CheckNumber(text, expectedValue);
 
     [Theory]
-    [InlineData("15", 15.0f)]
-    [InlineData("-743923", -743923.0f)]
-    [InlineData("482.392.923", 482_392_923.0f)]
-    [InlineData("21,500,000", 21_500_000.0f)]
+    [MemberData(nameof(NumbersWithDecimalComma))]
+    public static void ParseFloatingPointNumberWithDecimalCommaAsSpan(string text, float expectedValue) =>
+        CheckNumberAsSpan(text, expectedValue);
+
+    public static readonly TheoryData<string, float> NumbersWithDecimalComma =
+        new ()
+        {
+            { "000,7832", 0.7832f },
+            { "-0,499", -0.499f },
+            { "40593,84", 40593.84f },
+            { "1.943.100,84", 1_943_100.84f }
+        };
+
+    [Theory]
+    [MemberData(nameof(IntegerNumbers))]
     public static void ParseInteger(string text, float expectedValue) =>
         CheckNumber(text, expectedValue);
+
+    [Theory]
+    [MemberData(nameof(IntegerNumbers))]
+    public static void ParseIntegerAsSpan(string text, float expectedValue) =>
+        CheckNumberAsSpan(text, expectedValue);
+
+    public static readonly TheoryData<string, float> IntegerNumbers =
+        new ()
+        {
+            { "15", 15.0f },
+            { "-743923", -743923.0f },
+            { "482.392.923", 482_392_923.0f },
+            { "21,500,000", 21_500_000.0f }
+        };
 
     private static void CheckNumber(string text, float expectedValue)
     {
@@ -41,12 +75,16 @@ public static class FloatParserTests
         parsedValue.Should().BeApproximately(expectedValue, Precision);
     }
 
+    private static void CheckNumberAsSpan(ReadOnlySpan<char> text, float expectedValue)
+    {
+        var result = FloatParser.TryParse(text, out var parsedValue);
+
+        result.Should().BeTrue();
+        parsedValue.Should().BeApproximately(expectedValue, Precision);
+    }
+
     [Theory]
-    [InlineData("Foo")]
-    [InlineData("Bar")]
-    [InlineData("")]
-    [InlineData(null)]
-    [InlineData("9392gk381")]
+    [MemberData(nameof(InvalidNumbers))]
     public static void InvalidNumber(string text)
     {
         var result = FloatParser.TryParse(text, out var actualValue);
@@ -54,4 +92,24 @@ public static class FloatParserTests
         result.Should().BeFalse();
         actualValue.Should().Be(default);
     }
+
+    [Theory]
+    [MemberData(nameof(InvalidNumbers))]
+    public static void InvalidNumberAsSpan(string text)
+    {
+        var result = FloatParser.TryParse(text.AsSpan(), out var actualValue);
+
+        result.Should().BeFalse();
+        actualValue.Should().Be(default);
+    }
+
+    public static readonly TheoryData<string?> InvalidNumbers =
+        new ()
+        {
+            "Foo",
+            "Bar",
+            "",
+            null,
+            "9392gk381",
+        };
 }
--- Code/Synnotech.Core/Parsing/FloatParser.cs
+++ Code/Synnotech.Core/Parsing/FloatParser.cs
@@ -66,4 +66,64 @@ public static class FloatParser
         var cultureInfo = result.ChooseCultureInfo();
         return float.TryParse(text, style, cultureInfo, out value);
     }
+
+#if NETSTANDARD2_1
+    /// <summary>
+    /// <para>
+    /// Tries to parse the specified text to a float value. This method will
+    /// check if the text uses the point character '.' or the comma character ','
+    /// as the decimal sign and will parse the text either with the invariant culture
+    /// or the German culture. <see cref="NumberStyles.Number" /> is used when parsing
+    /// the text.
+    /// </para>
+    /// <para>
+    /// BE CAREFUL: if you have a number with only a single thousand-delimiter sign (i.e. no
+    /// decimal sign), this number will not be parsed correctly. The thousand-delimiter
+    /// sign will be interpreted as the decimal sign by this method. We recognize that
+    /// this scenario is rare, as especially human input will most likely never use
+    /// the thousand-delimiter sign, but if you happen to have this scenario, please
+    /// use <see cref="float.TryParse(string, NumberStyles, IFormatProvider, out float)" />
+    /// instead and specify the corresponding culture info.
+    /// </para>
+    /// </summary>
+    /// <param name="text">The text to be parsed.</param>
+    /// <param name="value">The parsed float value.</param>
+    /// <returns>True if parsing was successful, else false.</returns>
+    public static bool TryParse(ReadOnlySpan<char> text, out float value) =>
+        TryParse(text, NumberStyles.Number, out value);
+
+    /// <summary>
+    /// <para>
+    /// Tries to parse the specified text to a float value. This method will
+    /// check if the text uses the point character '.' or the comma character ','
+    /// as the decimal sign and will parse the text either with the invariant culture
+    /// or the German culture.
+    /// </para>
+    /// <para>
+    /// BE CAREFUL: if you have a number with only a single thousand-delimiter sign (i.e. no
+    /// decimal sign), this number will not be parsed correctly. The thousand-delimiter
+    /// sign will be interpreted as the decimal sign by this method. We recognize that
+    /// this scenario is rare, as especially human input will most likely never use
+    /// the thousand-delimiter sign, but if you happen to have this scenario, please
+    /// use <see cref="float.TryParse(string, NumberStyles, IFormatProvider, out float)" />
+    /// instead and specify the corresponding culture info.
+    /// </para>
+    /// </summary>
+    /// <param name="text">The text to be parsed.</param>
+    /// <param name="style">The number style that describes how the parser will handle the text.</param>
+    /// <param name="value">The parsed float value.</param>
+    /// <returns>True if parsing was successful, else false.</returns>
+    public static bool TryParse(ReadOnlySpan<char> text, NumberStyles style, out float value)
+    {
+        if (text.IsWhiteSpace())
+        {
+            value = default;
+            return false;
+        }
+
+        var result = FloatingPointAnalysis.AnalyseText(text);
+        var cultureInfo = result.ChooseCultureInfo();
+        return float.TryParse(text, style, cultureInfo, out value);
+    }
+#endif
 }
