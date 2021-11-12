7134b8529aaa1f6aeb70aba26f2288865ee666b9
Kenny Pflug <kenny.pflug@live.de>
2021-11-12 08:15:12 +0100

    GuidEntity now has a static property for configuring whether EmptyGuids are allowed

---
+++ Code/Synnotech.Core.Tests/Entities/GuidEntityTests.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using FluentAssertions;
+using Light.GuardClauses.Exceptions;
+using Synnotech.Core.Entities;
+using Xunit;
+
+// I will need to compare sometimes variables to themselves in these tests
+// ReSharper disable EqualExpressionComparison
+#pragma warning disable CS1718 // Comparison made to same variable
+
+namespace Synnotech.Core.Tests.Entities;
+
+public static class GuidEntityTests
+{
+    [Fact]
+    public static void MustImplementIEntityOfGuid() =>
+        typeof(GuidEntity<>).Should().Implement<IEntity<Guid>>();
+
+    [Fact]
+    public static void TheSameInstanceShouldBeConsideredEqual()
+    {
+        var entity = new Entity(Guid.Parse("F46369E3-8318-4B36-BA91-641E57D99ADB"));
+
+        (entity == entity).Should().BeTrue();
+        (entity != entity).Should().BeFalse();
+        entity.GetHashCode().Should().Be(entity.GetHashCode());
+    }
+
+    [Fact]
+    public static void TwoInstancesWithTheSameIdShouldBeEqual()
+    {
+        var guid = Guid.Parse("9CBF4BEA-B01B-4C23-A576-C19CC4F0C72A");
+        var x = new Entity(guid);
+        var y = new Entity(guid);
+
+        (x == y).Should().BeTrue();
+        (x != y).Should().BeFalse();
+        x.GetHashCode().Should().Be(y.GetHashCode());
+    }
+
+    [Fact]
+    public static void TwoInstancesWithDifferentIdsShouldNotBeEqual()
+    {
+        var x = new Entity(Guid.Parse("A0E5F557-82E4-480A-B6E3-FD2E62B4A203"));
+        var y = new Entity(Guid.Parse("B4AFC3C0-110A-41C3-84F6-4FC64C2E3470"));
+
+        (x == y).Should().BeFalse();
+        (x != y).Should().BeTrue();
+        x.GetHashCode().Should().NotBe(y.GetHashCode());
+    }
+
+    [Fact]
+    public static void ComparingWithNullMustReturnFalse()
+    {
+        var x = new Entity(Guid.Parse("CA015726-344D-4903-88B6-EBC0D21F2F20"));
+        var y = default(Entity);
+
+        (x == y).Should().BeFalse();
+        (x != y).Should().BeTrue();
+        (y == x).Should().BeFalse();
+        (y != x).Should().BeTrue();
+    }
+
+    [Fact]
+    public static void TwoNullReferencesAreEqual()
+    {
+        var @null = default(Entity);
+
+        (@null == @null).Should().BeTrue();
+        (@null != @null).Should().BeFalse();
+    }
+
+    // I need to test some constructors without ever needing the created instance
+    // ReSharper disable ObjectCreationAsStatement
+#pragma warning disable CA1806 // Do not ignore method results
+
+    [Fact]
+    public static void EmptyGuidShouldThrowByDefaultViaConstructor()
+    {
+        Action act = () => new Entity(Guid.Empty);
+
+        act.Should().Throw<EmptyGuidException>()
+           .And.ParamName.Should().Be("id");
+    }
+
+    [Fact]
+    public static void EmptyGuidShouldThrowByDefaultViaPropertyInitialization()
+    {
+        Action act = () => new Entity { Id = Guid.Empty };
+
+        act.Should().Throw<EmptyGuidException>();
+    }
+
+
+    [Fact]
+    public static void AllowEmptyGuidViaConstructor()
+    {
+        Entity.AllowEmptyGuid = true;
+
+        new Entity(Guid.Empty);
+
+        Entity.AllowEmptyGuid = false;
+    }
+
+    [Fact]
+    public static void AllowEmptyGuidViaPropertyInitialization()
+    {
+        Entity.AllowEmptyGuid = true;
+
+        new Entity { Id = Guid.Empty };
+
+        Entity.AllowEmptyGuid = false;
+    }
+
+    // ReSharper restore ObjectCreationAsStatement
+#pragma warning restore CA1806
+
+    [Theory]
+    [InlineData("7202ACA4-05C7-4170-A19A-8330BF184835")]
+    [InlineData("946111C5-0EC5-47DD-9B77-13AED3B8C42C")]
+    [InlineData("9C8277DA-5856-46FE-9299-80B8BCA9C4FC")]
+    public static void ToStringShouldReturnSimpleTypeNameAndId(string guidText)
+    {
+        var guid = Guid.Parse(guidText);
+        new Entity(guid).ToString().Should().Be("Entity " + guid);
+    }
+
+    private sealed class Entity : GuidEntity<Entity>
+    {
+        public Entity() { }
+
+        public Entity(Guid id) : base(id) { }
+    }
+}
---
+++ Code/Synnotech.Core/Entities/GuidEntity.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using Light.GuardClauses;
+using Light.GuardClauses.Exceptions;
+
+namespace Synnotech.Core.Entities;
+
+/// <summary>
+/// Represents the base class for entities that have a <see cref="Guid" /> ID.
+/// This class implements <see cref="IEquatable{T}" /> so that two instances with
+/// the same ID are considered equal. If you want to check for reference equality,
+/// you must explicitly call <see cref="object.ReferenceEquals" />.
+/// </summary>
+/// <typeparam name="T">
+/// Your type that derives from this class. This generic parameter is used for
+/// the <see cref="IEquatable{T}" /> implementation.
+/// </typeparam>
+public abstract class GuidEntity<T> : IEntity<Guid>, IEquatable<T>
+    where T : GuidEntity<T>
+{
+    private readonly Guid _id;
+
+    /// <summary>
+    /// Initializes a new instance of <see cref="GuidEntity{T}" />.
+    /// </summary>
+    protected GuidEntity() { }
+
+    /// <summary>
+    /// Initializes a new instance of <see cref="GuidEntity{T}" /> with the specified ID.
+    /// </summary>
+    /// <param name="id">The ID for the entity.</param>
+    /// <exception cref="EmptyGuidException">
+    /// Thrown when <paramref name="id" /> is an empty GUID and <see cref="AllowEmptyGuid" />
+    /// is set to false (which is the default value).
+    /// </exception>
+    protected GuidEntity(Guid id)
+    {
+        if (!AllowEmptyGuid)
+            id.MustNotBeEmpty(nameof(id));
+        _id = id;
+    }
+
+    /// <summary>
+    /// Gets or sets the value indicating whether empty GUIDs can be set on this entity type.
+    /// The default value is false. You should only set this property once at program startup
+    /// (in your composition root) for the corresponding entity type.
+    /// </summary>
+    // ReSharper disable once StaticMemberInGenericType -- I actually want this static field present for each generic type
+    public static bool AllowEmptyGuid { get; set; }
+
+    /// <inheritdoc />
+    public Guid Id
+    {
+        get => _id;
+        init
+        {
+            if (!AllowEmptyGuid)
+                value.MustNotBeEmpty();
+            _id = value;
+        }
+    }
+
+    /// <summary>
+    /// Checks if the other instance is not null and has the same ID as this instance.
+    /// </summary>
+    /// <returns>True when both entities are considered equal, else false.</returns>
+    public bool Equals(T? other) =>
+        other is not null && Id == other.Id;
+
+    /// <summary>
+    /// Checks if the other instance is of the same entity type and has the same ID as this instance.
+    /// </summary>
+    /// <returns>True when both entities are considered equal, else false.</returns>
+    public override bool Equals(object @object) =>
+        @object is T entity && Equals(entity);
+
+    /// <summary>
+    /// Returns the Id of the entity.
+    /// </summary>
+    public override int GetHashCode() => Id.GetHashCode();
+
+    /// <summary>
+    /// Returns the simple type name and appends the ID.
+    /// </summary>
+    public override string ToString() => $"{GetType().Name} {Id}";
+
+    /// <summary>
+    /// Checks if two entities are equal.
+    /// </summary>
+    public static bool operator ==(GuidEntity<T>? left, T? right) => left?.Equals(right) ?? right is null;
+
+    /// <summary>
+    /// Checks if two entities are not equal.
+    /// </summary>
+    public static bool operator !=(GuidEntity<T>? left, T? right) => !(left?.Equals(right) ?? right is null);
+}
+
+/// <summary>
+/// Represents a specialized base class for entities that have an <see cref="Guid" /> ID.
+/// Consider deriving from <see cref="GuidEntity{T}" /> if you want to limit which
+/// entities can be compared with each other.
+/// </summary>
+public abstract class GuidEntity : GuidEntity<GuidEntity>
+{
+    /// <summary>
+    /// Initializes a new instance of <see cref="GuidEntity" />
+    /// </summary>
+    protected GuidEntity() { }
+
+    /// <summary>
+    /// Initializes a new instance of <see cref="GuidEntity" /> with the specified ID.
+    /// </summary>
+    /// <param name="id">The ID for the entity.</param>
+    protected GuidEntity(Guid id) : base(id) { }
+}
